9117373953aaebb9c4281954cab94d9cfcfc1c34
llixtar <shelakov92@gmail.com>
2026-04-09 23:09:39 +0200

    fix: wrap checkout page in Suspense for Vercel build

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { CheckoutForm } from '@/components/checkout/CheckoutForm';
 import { OrderSummary } from '@/components/checkout/OrderSummary';
 import type { Metadata } from 'next';
@@ -25,17 +26,23 @@ export default function CheckoutPage() {
           Оформлення замовлення
         </h1>
 
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-start">
-          {/* Форма */}
-          <div className="lg:col-span-7 xl:col-span-8">
-            <CheckoutForm />
+        <Suspense fallback={
+          <div className="flex items-center justify-center min-h-[400px]">
+            <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-bottle"></div>
           </div>
+        }>
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-start">
+            {/* Форма */}
+            <div className="lg:col-span-7 xl:col-span-8">
+              <CheckoutForm />
+            </div>
 
-          {/* Підсумок */}
-          <div className="lg:col-span-5 xl:col-span-4">
-            <OrderSummary />
+            {/* Підсумок */}
+            <div className="lg:col-span-5 xl:col-span-4">
+              <OrderSummary />
+            </div>
           </div>
-        </div>
+        </Suspense>
       </div>
     </div>
   );
